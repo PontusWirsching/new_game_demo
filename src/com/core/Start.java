26eca59e8970e749e4fd7c35da8fc1aba15e5d37
Pontus <flash13records@gmail.com>
2014-09-17 21:16:47 +0200

    Added start class and engine
    Signed-off-by: Pontus <flash13records@gmail.com>

---
+++ src/com/core/Start.java
@@ -0,0 +1,27 @@
+package com.core;
+
+import com.engine.LEngine;
+
+public class Start extends LEngine {
+
+	public Start(int width, int height, String title) {
+		super(width, height, title);
+		
+		start();
+	}
+
+	@Override
+	public void update() {
+
+	}
+	
+	@Override
+	public void render() {
+
+	}
+
+	public static void main(String[] args) {
+		new Start(1024, 720, "Game Demo");
+	}
+
+}
