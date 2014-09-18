ca3e024a9902ca443b7fed666fb2e2c89f9ad9ac
Pontus <flash13records@gmail.com>
2014-09-18 22:14:21 +0200

    Added inventory texture
    Signed-off-by: Pontus <flash13records@gmail.com>

--- src/com/core/tools/Textures.java
+++ src/com/core/tools/Textures.java
@@ -5,14 +5,14 @@ import com.engine.graphics.SpriteSheet;
 public class Textures {
 
 	public static SpriteSheet items;
-	
+	public static SpriteSheet guis;
+
 	public static void load() {
 		
 		items = new SpriteSheet("/textures/items.png");
-		
-		items.loadSprite("ENGINE_UPGRADE", 0, 0, 32, 32);
-		items.loadSprite("STORAGE_UPGRADE", 32, 0, 32, 32);
+		guis = new SpriteSheet("/textures/gui.png");
 
+		
 		
 	}
 	
